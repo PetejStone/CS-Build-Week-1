6c444b9fc0519c6a784deaf5a29ad128b17492bb
Erik Sandoval <erik.sandoval@icloud.com>
2019-09-23 20:41:23 -0700

    testing out the new model

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'adventure',
+    'adventure.apps.AdventureConfig',
     'api',
     'corsheaders',
     'rest_framework',
